b1548783d82b281ebafbf6350fe23edd9593a7a8
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-07-16 17:10:11 +0530

    Update job sequencing.c

--- job sequencing.c
+++ job sequencing.c
@@ -1,1 +1,70 @@
+#include <stdio.h>
 
+typedef struct {
+    int id;
+    int deadline;
+    int profit;
+} Job;
+
+int findMaxDeadline(Job jobs[], int n) {
+    int maxDeadline = jobs[0].deadline;
+    for (int i = 1; i < n; i++) {
+        if (jobs[i].deadline > maxDeadline) {
+            maxDeadline = jobs[i].deadline;
+        }
+    }
+    return maxDeadline;
+}
+
+void jobSequencing(Job jobs[], int n) {
+    int maxDeadline = findMaxDeadline(jobs, n);
+    int slots[maxDeadline];
+    for (int i = 0; i < maxDeadline; i++) {
+        slots[i] = -1;
+    }
+    
+    int totalProfit = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = jobs[i].deadline - 1; j >= 0; j--) {
+            if (slots[j] == -1) {
+                slots[j] = jobs[i].id;
+                totalProfit += jobs[i].profit;
+                break;
+            }
+        }
+    }
+
+    printf("Job sequence: ");
+    for (int i = 0; i < maxDeadline; i++) {
+        if (slots[i] != -1) {
+            printf("Job%d ", slots[i]);
+        }
+    }
+    printf("\nTotal Profit: %d\n", totalProfit);
+}
+
+int main() {
+    int n;
+    printf("Enter number of jobs: ");
+    scanf("%d", &n);
+    Job jobs[n];
+    
+    for (int i = 0; i < n; i++) {
+        printf("Enter job id, deadline and profit for job %d: ", i + 1);
+        scanf("%d %d %d", &jobs[i].id, &jobs[i].deadline, &jobs[i].profit);
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (jobs[i].profit < jobs[j].profit) {
+                Job temp = jobs[i];
+                jobs[i] = jobs[j];
+                jobs[j] = temp;
+            }
+        }
+    }
+
+    jobSequencing(jobs, n);
+
+    return 0;
+}
