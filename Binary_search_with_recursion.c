9eaa39f6aab875561b590f668127e40953c66f0d
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-08-02 15:09:04 +0530

    Update Binary_search_with_recursion.c

--- Binary_search_with_recursion.c
+++ Binary_search_with_recursion.c
@@ -8,7 +8,7 @@ int binary_search(int arr[], int low, int high, int target) {
  //using mid
  int mid = (low + high) / 2;
 
- //checking condit
+ //checking conditi
  if (arr[mid] == target) {
  return mid;
  }
