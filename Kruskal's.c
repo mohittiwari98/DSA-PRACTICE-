b1263c7713914ed3060bfc715f21b116b7069486
mohittiwari98 <mohittiwari17091709@gmail.com>
2025-08-10 19:38:24 +0530

    Update Kruskal's.c

--- Kruskal's.c
+++ Kruskal's.c
@@ -10,7 +10,7 @@ typedef struct {
 typedef struct {
     int parent, rank;
 } Subset;
-//compar
+//compare
 int compareEdges(const void *a, const void *b) {
     return ((Edge *)a)->weight - ((Edge *)b)->weight;
 }
